68c01b1200da142d0a5a0133f25807d05044be47
Pulkit Rana <pulkit.rana221@gmail.com>
2025-06-26 00:41:18 +0530

    improved auth
    fixes

--- src/main/java/com/syncNest/user_management/UserManagementApplication.java
+++ src/main/java/com/syncNest/user_management/UserManagementApplication.java
@@ -2,10 +2,12 @@ package com.syncNest.user_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ComponentScan(basePackages = {"com.syncNest.user_management", "com.syncNest.user_management.security"})
 public class UserManagementApplication {
 
     public static void main(String[] args) {
